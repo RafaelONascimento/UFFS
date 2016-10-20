0686b58ce7ee0627439ec216fbc113b0a4da7eb4
= <=>
2016-10-20 15:24:43 -0200

    Alteracao da matriz dos valores, para float

--- Algebra Linear/Gram-Schmidt/Gram-Schmidt.cpp
+++ Algebra Linear/Gram-Schmidt/Gram-Schmidt.cpp
@@ -6,7 +6,7 @@
 
 int dimensao;
 
-int verificaLD(int matrizVetores[MAX][MAX]){
+int verificaLD(float matrizVetores[MAX][MAX]){
   int flag = false;
   float a;
   
@@ -24,7 +24,7 @@ int verificaLD(int matrizVetores[MAX][MAX]){
   return flag;
 }
 
-void produtoInterno(int v[], int u[], int proj[]){
+void produtoInterno(float v[], float u[], float proj[]){
   float numerador = 0, denominador = 0,escalar = 0;
 
   for(int i = 0; i < dimensao; i++)
@@ -33,28 +33,29 @@ void produtoInterno(int v[], int u[], int proj[]){
     denominador += u[i]*u[i];
 
   escalar = (numerador/denominador);
-
+  printf("%f\n",escalar);
   for(int i = 0; i < dimensao; i++)
     proj[i] = u[i]*escalar;
 }
 
-void gramSchmidt(int matrizVetores[MAX][MAX]){
-  int proj[dimensao];
+void gramSchmidt(float matrizVetores[MAX][MAX]){
+  float proj[dimensao];
   
   for(int i = 1; i < dimensao; i++){
     for(int j =0; j <= (i-1); j++){
       produtoInterno(matrizVetores[i],matrizVetores[j],proj);
+      
       for(int k = 0; k < dimensao; k++)
-	matrizVetores[i] = matrizVetores[i][k] - proj[k];
+	matrizVetores[i][k] = matrizVetores[i][k] - proj[k];
     }
   }
   
 }
 
-int leituraArquivo(int matrizVetores[MAX][MAX]){
-  int result, l = 0,c = 0, flag = 0;
-      
+int leituraArquivo(float matrizVetores[MAX][MAX]){
+  int l = 0,c = 0, flag = 0,result;
   FILE *arq = fopen(URL, "rt");
+
   if (!arq){
     printf("NAO FOI POSSIVEL ABRIR O ARQUIVO\n");
     return 0;
@@ -64,8 +65,11 @@ int leituraArquivo(int matrizVetores[MAX][MAX]){
     result = fgetc(arq);
     if(result >= 48 &&  result <= 57){
       matrizVetores[l][c] = (result-48);
+      printf("%d\n",(result-48));
       c++;
     }
+
+    
     
     if(result == ')' && c){
       dimensao = c;
@@ -78,15 +82,23 @@ int leituraArquivo(int matrizVetores[MAX][MAX]){
 }
 
 int main(){
-  int matrizVetores[MAX][MAX];
+  float matrizVetores[MAX][MAX];
   leituraArquivo(matrizVetores);
-
-  
-  
   
   for(int l= 0; l< dimensao; l++){
     for(int c = 0; c < dimensao; c++){
-      printf("%d ", matrizVetores[l][c]);
+      printf("%f ", matrizVetores[l][c]);
+    }
+    printf("\n");
+  }
+
+  printf("-------------------\n");
+
+  gramSchmidt(matrizVetores);
+  
+  for(int l= 0; l< dimensao; l++){
+    for(int c = 0; c < dimensao; c++){
+      printf("%f ", matrizVetores[l][c]);
     }
     printf("\n");
   }
